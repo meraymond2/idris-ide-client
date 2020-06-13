b0972c0cbddbc442402eee558c0e590834f4e512
Michael Raymond <michael_raymond@fastmail.com>
2020-06-13 12:11:51 +0100

    Only build src dir

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import { InfoReply, FinalReply, OutputReply, Reply } from "./reply"
 import { Request } from "./request"
 
 export { IdrisClient, InfoReply, FinalReply, OutputReply, Reply, Request }
+
+console.log("hey")
