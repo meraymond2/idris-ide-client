9434209a95700ee4221c986cb4cb1bf7f03c038a
Michael Raymond <michael_raymond@fastmail.com>
2020-06-13 13:21:18 +0100

    client instantiation examples

---
+++ examples/socket.ts
@@ -0,0 +1,22 @@
+import { spawn } from "child_process"
+import { IdrisClient } from "idris-ide-client"
+import { Socket } from "net"
+
+const idrisProc = spawn("idris", ["--ide-mode-socket"])
+
+// When you start Idris in socket IDE mode, it prints the socket port.
+const portProm: Promise<number> = new Promise((res) =>
+  idrisProc.stdout.once("data", (chunk: Buffer) =>
+    res(parseInt(chunk.toString("utf8")))
+  )
+)
+const socket = new Socket({ readable: true, writable: true })
+
+const port = await portProm
+socket.connect({ port })
+
+const client = new IdrisClient(socket, socket)
+client
+  .interpret("2 + 2")
+  .then(console.log)
+  .then(() => socket.destroy())
