42f02c6077d4d524330bb9fcab06d30538065092
Michael Raymond <michael_raymond@fastmail.com>
2020-06-23 09:46:23 +0100

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import { IdrisClient } from "./client"
-import { InfoReply, FinalReply, OutputReply, Reply } from "./reply"
+import {
+  InfoReply,
+  FinalReply,
+  HolePremise,
+  Metavariable,
+  MessageMetadata,
+  OutputReply,
+  Reply,
+  SourceMetadata,
+} from "./reply"
 import { Request } from "./request"
+import { Decor } from "./s-exps"
 
-export { IdrisClient, InfoReply, FinalReply, OutputReply, Reply, Request }
+export {
+  Decor,
+  FinalReply,
+  IdrisClient,
+  InfoReply,
+  HolePremise,
+  MessageMetadata,
+  Metavariable,
+  OutputReply,
+  Reply,
+  Request,
+  SourceMetadata,
+}
