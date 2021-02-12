62a3cf9e351e1a6ce42f0d96c04633be5b62bd71
Michael Raymond <raymond.michael@gmail.com>
2021-02-12 15:14:58 +0000

    Add Idris 2 examples

--- examples/socket.ts
+++ examples/socket.ts
@@ -2,21 +2,49 @@ import { spawn } from "child_process"
 import { IdrisClient } from "idris-ide-client"
 import { Socket } from "net"
 
-const idrisProc = spawn("idris", ["--ide-mode-socket"])
+/* Idris 1 */
+{
+  const idrisProc = spawn("idris", ["--ide-mode-socket"])
 
-// When you start Idris in socket IDE mode, it prints the socket port.
-const portProm: Promise<number> = new Promise((res) =>
-  idrisProc.stdout.once("data", (chunk: Buffer) =>
-    res(parseInt(chunk.toString("utf8")))
+  // When you start Idris in socket IDE mode, it prints the socket port.
+  const portProm: Promise<number> = new Promise((res) =>
+    idrisProc.stdout.once("data", (chunk: Buffer) =>
+      res(parseInt(chunk.toString("utf8")))
+    )
   )
-)
-const socket = new Socket({ readable: true, writable: true })
+  const socket = new Socket({ readable: true, writable: true })
 
-const port = await portProm
-socket.connect({ port })
+  const port = await portProm
+  socket.connect({ port })
 
-const client = new IdrisClient(socket, socket)
-client
-  .interpret("2 + 2")
-  .then(console.log)
-  .then(() => socket.destroy())
+  const client = new IdrisClient(socket, socket)
+  client
+    .interpret("2 + 2")
+    .then(console.log)
+    .then(() => socket.destroy())
+}
+
+/* Idris 2 */
+{
+  const idrisProc = spawn("idris2", ["--ide-mode-socket", "--find-ipkg"])
+
+  // When you start Idris in socket IDE mode, it prints the socket port.
+  const portProm: Promise<number> = new Promise((res) =>
+    idrisProc.stdout.once("data", (chunk: Buffer) =>
+      res(parseInt(chunk.toString("utf8")))
+    )
+  )
+  const socket = new Socket({ readable: true, writable: true })
+
+  const port = await portProm
+  socket.connect({ port })
+
+  const client = new IdrisClient(socket, socket)
+  client
+    .interpret("2 + 2")
+    .then(console.log)
+    .then(() => {
+      client.close()
+      socket.destroy()
+    })
+}
