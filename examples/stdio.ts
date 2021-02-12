71921072668d57e86db3bcc42851391fae62a07b
Michael Raymond <raymond.michael@gmail.com>
2021-02-12 15:14:58 +0000

    Add Idris 2 examples

--- examples/stdio.ts
+++ examples/stdio.ts
@@ -1,10 +1,27 @@
 import { spawn } from "child_process"
 import { IdrisClient } from "idris-ide-client"
 
-const idrisProc = spawn("idris", ["--ide-mode"])
-const client = new IdrisClient(idrisProc.stdin, idrisProc.stdout)
+/* Idris 1 */
+{
+  const idrisProc = spawn("idris", ["--ide-mode"])
+  const client = new IdrisClient(idrisProc.stdin, idrisProc.stdout)
 
-client
-  .interpret("2 + 2")
-  .then(console.log)
-  .then(() => idrisProc.kill())
+  client
+    .interpret("2 + 2")
+    .then(console.log)
+    .then(() => idrisProc.kill())
+}
+
+/* Idris 2 */
+{
+  const idrisProc = spawn("idris2", ["--ide-mode", "--find-ipkg"])
+  const client = new IdrisClient(idrisProc.stdin, idrisProc.stdout)
+
+  client
+    .iterpret("2 + 2")
+    .then(console.log)
+    .then(() => {
+      client.close()
+      idrisProc.kill()
+    })
+}
